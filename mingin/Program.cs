6638e11736a621c0e52aed76a26d41e167c5dc67
ASUS <ASUS@DESKTOP-UEHIM0J>
2023-10-31 22:05:36 +0330

    Add project files.

---
+++ mingin/Program.cs
@@ -0,0 +1,70 @@
+﻿//tamrin 3 -- mingin 10 nomre  --  amir mahdi hosein njad
+do
+{
+    Console.WriteLine("nomre fizik ");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    a = a * 3;
+    Console.WriteLine("nomre shimi ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    b = b * 2;
+
+    Console.WriteLine("nomre hesaban ");
+    int c = Convert.ToInt32(Console.ReadLine());
+
+    c = c * 2;
+
+    Console.WriteLine("nomre hendese ");
+    int d = Convert.ToInt32(Console.ReadLine());
+
+    d = d * 1;
+
+    Console.WriteLine("nomre gosaste ");
+    int e = Convert.ToInt32(Console.ReadLine());
+
+    e = e * 1;
+
+    Console.WriteLine("nomre adabiat");
+    int f = Convert.ToInt32(Console.ReadLine());
+
+    f = f * 3;
+
+    Console.WriteLine("nomre arabi");
+    int g = Convert.ToInt32(Console.ReadLine());
+
+    g = g * 2;
+
+    Console.WriteLine("nomre dini");
+    int h = Convert.ToInt32(Console.ReadLine());
+
+    h = h * 1;
+
+    Console.WriteLine("nomre zaban");
+    int i = Convert.ToInt32(Console.ReadLine());
+
+    i = i * 2;
+
+    Console.WriteLine("nomre tafsir ghoran");
+    int k = Convert.ToInt32(Console.ReadLine());
+
+    k = k * 2;
+
+    int sum = (a + b + c + d + e + f + g + h + i + k) / 19;
+
+    Console.Write("mingin nomarat danshjoo = ");
+    Console.WriteLine(sum);
+
+    Console.WriteLine("mikhay dobare miangin begiry?y or n");
+} while (Console.ReadLine() == "y") ;
+Console.WriteLine("------payan------");
+
+
+
+
+
+
+
+
+
+
